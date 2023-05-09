79c3ff23067ef0afbcbd14509c7d199fbef7db61
Marek Dědič <developer@dedic.eu>
2023-05-09 17:02:13 +0200

    Fixed KeymapBuilder not supporting multiple keymaps for the same key

--- src/prosemirror-unified/KeymapBuilder.ts
+++ src/prosemirror-unified/KeymapBuilder.ts
@@ -1,22 +1,34 @@
-import { baseKeymap } from "prosemirror-commands";
+import { baseKeymap, chainCommands } from "prosemirror-commands";
 import { keymap } from "prosemirror-keymap";
 import type { Command, Plugin } from "prosemirror-state";
 
 import type { ExtensionManager } from "./ExtensionManager";
 
 export class KeymapBuilder {
-  private readonly keymap: Record<string, Command>;
+  private readonly keymap: Map<string, Array<Command>>;
 
   public constructor(extensionManager: ExtensionManager) {
-    this.keymap = Object.assign(
-      baseKeymap,
-      ...extensionManager
-        .syntaxExtensions()
-        .map((extension) => extension.proseMirrorKeymap())
-    ) as Record<string, Command>;
+    this.keymap = new Map();
+    this.addKeymap(baseKeymap);
+    for (const extension of extensionManager.syntaxExtensions()) {
+      this.addKeymap(extension.proseMirrorKeymap());
+    }
   }
 
   public build(): Plugin {
-    return keymap(this.keymap);
+    const chainedKeymap: Record<string, Command> = {};
+    this.keymap.forEach((commands, key) => {
+      chainedKeymap[key] = chainCommands(...commands);
+    });
+    return keymap(chainedKeymap);
+  }
+
+  private addKeymap(map: Record<string, Command>): void {
+    for (const key in map) {
+      if (!this.keymap.get(key)) {
+        this.keymap.set(key, []);
+      }
+      this.keymap.get(key)!.push(map[key]);
+    }
   }
 }
