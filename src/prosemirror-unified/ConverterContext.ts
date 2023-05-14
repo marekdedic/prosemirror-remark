574657dcc1fa4fae669b5cd3a5df33e7d2a93007
Marek Dědič <developer@dedic.eu>
2023-05-14 22:53:25 +0200

    Removed local copy of prosemirror-unified

--- src/prosemirror-unified/ConverterContext.ts
+++
@@ -1,1 +0,0 @@
-export type ConverterContext<T> = Partial<T>;
--- src/prosemirror-unified/index.ts
+++
@@ -1,6 +0,0 @@
-export { ConverterContext } from "./ConverterContext";
-export { Extension } from "./Extension";
-export { MarkExtension } from "./MarkExtension";
-export { MarkInputRule } from "./MarkInputRule";
-export { NodeExtension } from "./NodeExtension";
-export { ProseMirrorUnified } from "./ProseMirrorUnified";
