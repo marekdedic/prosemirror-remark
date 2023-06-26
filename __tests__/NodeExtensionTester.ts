9f2f203db0323a3b891ee0be0df38d5fb18cdd16
Marek Dědič <developer@dedic.eu>
2023-06-26 22:01:03 +0200

    Split off SyntaxExtensionTester

--- __tests__/NodeExtensionTester.ts
+++ __tests__/NodeExtensionTester.ts
@@ -1,14 +1,15 @@
 import type { NodeExtension } from "prosemirror-unified";
 import type { Node as UnistNode } from "unist";
 
-interface NodeExtensionTesterConfig {
+import {
+  SyntaxExtensionTester,
+  type SyntaxExtensionTesterConfig,
+} from "./SyntaxExtensionTester";
+
+interface NodeExtensionTesterConfig extends SyntaxExtensionTesterConfig {
   unistNodeName: string;
 }
 
-// TODO: Test input rules
-// TODO: Test keymap
-// TODO: Test post-hook
-// TODO: Test unist -> ProseMirror
 // TODO: Add things from NodeExtension
 
 // TODO: Re-evaluate
@@ -19,53 +20,22 @@ export class NodeExtensionTester<
     string,
     never
   >
-> {
-  private readonly extension: NodeExtension<UNode, UnistToProseMirrorContext>;
-  private readonly unistNodeName: string;
-
-  private readonly unistNodeMatches: Array<{
-    node: UnistNode;
-    shouldMatch: boolean;
-  }>;
-
+> extends SyntaxExtensionTester<UNode, UnistToProseMirrorContext> {
   public constructor(
     extension: NodeExtension<UNode, UnistToProseMirrorContext>,
     config: NodeExtensionTesterConfig
   ) {
-    this.extension = extension;
-    this.unistNodeName = config.unistNodeName;
-
-    this.unistNodeMatches = [];
-  }
-
-  public shouldMatchUnistNode(node: UnistNode): this {
-    this.unistNodeMatches.push({ node, shouldMatch: true });
-    return this;
-  }
-
-  public shouldNotMatchUnistNode(node: UnistNode): this {
-    this.unistNodeMatches.push({ node, shouldMatch: false });
-    return this;
+    super(extension, config);
   }
 
   public test(): void {
     // eslint-disable-next-line jest/valid-title -- The rule requires a string literal
     describe(this.extension.constructor.name, () => {
-      test("Handles the correct unist node", () => {
-        expect(this.extension.unistNodeName()).toBe(this.unistNodeName);
-      });
-
-      if (this.unistNodeMatches.length > 0) {
-        test("Matches correct unist nodes", () => {
-          // eslint-disable-next-line jest/prefer-expect-assertions -- The rule requires a number literal
-          expect.assertions(this.unistNodeMatches.length);
-          for (const { node, shouldMatch } of this.unistNodeMatches) {
-            expect(this.extension.unistToProseMirrorTest(node)).toBe(
-              shouldMatch
-            );
-          }
-        });
-      }
+      this.enqueueTests();
     });
+  }
+
+  protected enqueueTests(): void {
+    super.enqueueTests();
   }
 }
---
+++ __tests__/SyntaxExtensionTester.ts
@@ -0,0 +1,70 @@
+import type { SyntaxExtension } from "prosemirror-unified";
+import type { Node as UnistNode } from "unist";
+
+// TODO: Re-evaluate
+// eslint-disable-next-line jest/no-export
+export interface SyntaxExtensionTesterConfig {
+  unistNodeName: string;
+}
+
+// TODO: Test input rules
+// TODO: Test keymap
+// TODO: Test post-hook
+// TODO: Test unist -> ProseMirror
+
+// TODO: Re-evaluate
+// eslint-disable-next-line jest/no-export
+export class SyntaxExtensionTester<
+  UNode extends UnistNode,
+  UnistToProseMirrorContext extends Record<string, unknown> = Record<
+    string,
+    never
+  >
+> {
+  protected readonly extension: SyntaxExtension<
+    UNode,
+    UnistToProseMirrorContext
+  >;
+  private readonly unistNodeName: string;
+
+  private readonly unistNodeMatches: Array<{
+    node: UnistNode;
+    shouldMatch: boolean;
+  }>;
+
+  public constructor(
+    extension: SyntaxExtension<UNode, UnistToProseMirrorContext>,
+    config: SyntaxExtensionTesterConfig
+  ) {
+    this.extension = extension;
+    this.unistNodeName = config.unistNodeName;
+
+    this.unistNodeMatches = [];
+  }
+
+  public shouldMatchUnistNode(node: UnistNode): this {
+    this.unistNodeMatches.push({ node, shouldMatch: true });
+    return this;
+  }
+
+  public shouldNotMatchUnistNode(node: UnistNode): this {
+    this.unistNodeMatches.push({ node, shouldMatch: false });
+    return this;
+  }
+
+  protected enqueueTests(): void {
+    test("Handles the correct unist node", () => {
+      expect(this.extension.unistNodeName()).toBe(this.unistNodeName);
+    });
+
+    if (this.unistNodeMatches.length > 0) {
+      test("Matches correct unist nodes", () => {
+        // eslint-disable-next-line jest/prefer-expect-assertions -- The rule requires a number literal
+        expect.assertions(this.unistNodeMatches.length);
+        for (const { node, shouldMatch } of this.unistNodeMatches) {
+          expect(this.extension.unistToProseMirrorTest(node)).toBe(shouldMatch);
+        }
+      });
+    }
+  }
+}
