b8d8a4aa1afd9b3b93fe6f3754131f58c1baefdc
Marek Dědič <developer@dedic.eu>
2023-06-26 19:26:22 +0200

    Turned off eslint rule jest/require-hook

--- __tests__/syntax-extensions/HorizontalRuleExtension.test.ts
+++ __tests__/syntax-extensions/HorizontalRuleExtension.test.ts
@@ -1,8 +1,6 @@
 import { HorizontalRuleExtension } from "../../src/syntax-extensions/HorizontalRuleExtension";
 import { NodeExtensionTester } from "../NodeExtensionTester";
 
-// TODO: Re-evalueate
-// eslint-disable-next-line jest/require-hook
 new NodeExtensionTester(new HorizontalRuleExtension(), {
   unistNodeName: "thematicBreak",
 })
