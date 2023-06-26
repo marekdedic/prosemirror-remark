c0cecb827facdc27cf62e002b0f5e2dfb3206308
Marek Dědič <developer@dedic.eu>
2023-06-26 10:03:18 +0200

    Added tests for unist node matching

--- __tests__/NodeExtensionTester.ts
+++ __tests__/NodeExtensionTester.ts
@@ -5,6 +5,12 @@ interface NodeExtensionTesterConfig {
   unistNodeName: string;
 }
 
+// TODO: Test input rules
+// TODO: Test keymap
+// TODO: Test post-hook
+// TODO: Test unist -> ProseMirror
+// TODO: Add things from NodeExtension
+
 // TODO: Re-evaluate
 // eslint-disable-next-line jest/no-export
 export class NodeExtensionTester<
@@ -17,12 +23,29 @@ export class NodeExtensionTester<
   private readonly extension: NodeExtension<UNode, UnistToProseMirrorContext>;
   private readonly unistNodeName: string;
 
+  private readonly unistNodeMatches: Array<{
+    node: UnistNode;
+    shouldMatch: boolean;
+  }>;
+
   public constructor(
     extension: NodeExtension<UNode, UnistToProseMirrorContext>,
     config: NodeExtensionTesterConfig
   ) {
     this.extension = extension;
     this.unistNodeName = config.unistNodeName;
+
+    this.unistNodeMatches = [];
+  }
+
+  public shouldMatchUnistNode(node: UnistNode): this {
+    this.unistNodeMatches.push({ node, shouldMatch: true });
+    return this;
+  }
+
+  public shouldNotMatchUnistNode(node: UnistNode): this {
+    this.unistNodeMatches.push({ node, shouldMatch: false });
+    return this;
   }
 
   public test(): void {
@@ -31,6 +54,18 @@ export class NodeExtensionTester<
       test("Handles the correct unist node", () => {
         expect(this.extension.unistNodeName()).toBe(this.unistNodeName);
       });
+
+      if (this.unistNodeMatches.length > 0) {
+        test("Matches correct unist nodes", () => {
+          // eslint-disable-next-line jest/prefer-expect-assertions -- The rule requires a number literal
+          expect.assertions(this.unistNodeMatches.length);
+          for (const { node, shouldMatch } of this.unistNodeMatches) {
+            expect(this.extension.unistToProseMirrorTest(node)).toBe(
+              shouldMatch
+            );
+          }
+        });
+      }
     });
   }
 }
--- __tests__/syntax-extensions/HorizontalRuleExtension.test.ts
+++ __tests__/syntax-extensions/HorizontalRuleExtension.test.ts
@@ -1,6 +1,12 @@
 import { HorizontalRuleExtension } from "../../src/syntax-extensions/HorizontalRuleExtension";
 import { NodeExtensionTester } from "../NodeExtensionTester";
 
+// TODO: Re-evalueate
+// eslint-disable-next-line jest/require-hook
 new NodeExtensionTester(new HorizontalRuleExtension(), {
   unistNodeName: "thematicBreak",
-}).test();
+})
+  .shouldMatchUnistNode({ type: "thematicBreak" })
+  .shouldNotMatchUnistNode({ type: "horizontal_rule" })
+  .shouldNotMatchUnistNode({ type: "other" })
+  .test();
