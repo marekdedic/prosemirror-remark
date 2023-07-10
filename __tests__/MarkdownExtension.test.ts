d1926b0accfe70aa0bb47717f105f7942f858eb0
Marek Dědič <developer@dedic.eu>
2023-07-10 15:59:32 +0200

    Added a second test for MarkdownExtension

--- __tests__/MarkdownExtension.test.ts
+++ __tests__/MarkdownExtension.test.ts
@@ -76,3 +76,71 @@ test("unist -> ProseMirror conversion", () => {
     ])!
   );
 });
+
+test("ProseMirror -> unist conversion", () => {
+  const pmu = new ProseMirrorUnified([new MarkdownExtension()]);
+  const schema = pmu.schema();
+  const result = pmu.serialize(
+    schema.nodes["doc"].createAndFill({}, [
+      schema.nodes["blockquote"].createAndFill({}, [
+        schema.nodes["paragraph"].createAndFill({}, [
+          schema.text("Inside a blockquote"),
+        ])!,
+      ])!,
+      schema.nodes["code_block"].createAndFill({}, [schema.text("Code")])!,
+      schema.nodes["heading"].createAndFill({}, [schema.text("Hello")])!,
+      schema.nodes["horizontal_rule"].createAndFill()!,
+      schema.nodes["paragraph"].createAndFill({}, [
+        schema.nodes["image"].createAndFill({
+          src: "https://example.test",
+          alt: "Awesome image",
+        })!,
+      ])!,
+      schema.nodes["ordered_list"].createAndFill({}, [
+        schema.nodes["list_item"].createAndFill({}, [
+          schema.nodes["paragraph"].createAndFill({}, [
+            schema.text("Ordered list"),
+          ])!,
+        ])!,
+      ])!,
+      schema.nodes["bullet_list"].createAndFill({}, [
+        schema.nodes["list_item"].createAndFill({}, [
+          schema.nodes["paragraph"].createAndFill({}, [
+            schema.text("Unordered list"),
+          ])!,
+        ])!,
+      ])!,
+      schema.nodes["paragraph"].createAndFill({}, [
+        schema.text("A text with a "),
+        schema.text("bold part").mark([schema.marks["strong"].create()]),
+        schema.text(", some "),
+        schema.text("inline code").mark([schema.marks["code"].create()]),
+        schema.text(", a bit "),
+        schema.text("that is italic").mark([schema.marks["em"].create()]),
+        schema.text(", one "),
+        schema
+          .text("link")
+          .mark([
+            schema.marks["link"].create({ href: "https://example.test" }),
+          ]),
+        schema.text("."),
+      ])!,
+    ])!
+  );
+  expect(result).toBe(
+    "> Inside a blockquote\n" +
+      "\n" +
+      "```\nCode\n```\n" +
+      "\n" +
+      "# Hello\n" +
+      "\n" +
+      "---\n" +
+      "\n" +
+      "![Awesome image](https://example.test)\n" +
+      "\n" +
+      "1.  Ordered list\n" +
+      "\n" +
+      "*   Unordered list\n\n" +
+      "A text with a **bold part**, some `inline code`, a bit *that is italic*, one [link](https://example.test).\n"
+  );
+});
