e2e54a76ace997c99e4abc980b4a1b8b2fbdbbb1
Marek Dědič <developer@dedic.eu>
2023-10-01 15:58:05 +0200

    Removed TODOs that are part of the PR

--- src/GFMExtension.ts
+++ src/GFMExtension.ts
@@ -7,12 +7,6 @@ import { MarkdownExtension } from "./MarkdownExtension";
 
 /**
  * @public
- *
- * TODO: Add support for tables
- * TODO: Add support for task list items
- * TODO: Add support for strikethrough
- * TODO: Add support for extended autolinks
- * TODO: Add support for filtered raw HTML
  */
 export class GFMExtension extends Extension {
   public dependencies(): Array<Extension> {
