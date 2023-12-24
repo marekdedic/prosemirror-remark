05c66c33607d4deba528c3385ec0e0c7265057b5
Marek Dědič <developer@dedic.eu>
2023-12-24 17:17:06 +0100

    Added StrikethroughExtension to GFM

--- src/GFMExtension.ts
+++ src/GFMExtension.ts
@@ -4,13 +4,17 @@ import type { Processor } from "unified";
 import type { Node as UnistNode } from "unist";
 
 import { MarkdownExtension } from "./MarkdownExtension";
+import { StrikethroughExtension } from "./syntax-extensions/StrikethroughExtension";
 
 /**
  * @public
  */
 export class GFMExtension extends Extension {
   public dependencies(): Array<Extension> {
-    return [new MarkdownExtension()];
+    return [
+      new StrikethroughExtension(),
+      new MarkdownExtension(),
+    ];
   }
 
   public unifiedInitializationHook(
