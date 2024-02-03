3db502defde56087c2863ee74cceb1f51306bb4c
Marek Dědič <developer@dedic.eu>
2024-02-03 21:42:06 +0100

    Testing keymap for bold

--- src/syntax-extensions/BoldExtension.ts
+++ src/syntax-extensions/BoldExtension.ts
@@ -53,7 +53,6 @@ export class BoldExtension extends MarkExtension<Strong> {
     ];
   }
 
-  // TODO: Test
   public override proseMirrorKeymap(
     proseMirrorSchema: Schema<string, string>,
   ): Record<string, Command> {
