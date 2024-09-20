8d2e845c66f1d53b0fac2f015d4bf71e8ca2cc91
Marek Dědič <developer@dedic.eu>
2024-09-20 19:35:50 +0200

    Separate tests for each case for mark input rules

--- __tests__/utils/MarkExtensionTester.ts
+++ __tests__/utils/MarkExtensionTester.ts
@@ -68,37 +68,38 @@ export class MarkExtensionTester<
       return;
     }
 
-    test("Matches input rules correctly", () => {
-      // eslint-disable-next-line jest/prefer-expect-assertions -- The rule requires a number literal
-      expect.assertions(3 * this.inputRuleMatches.length);
+    describe("Matches input rules correctly", () => {
+      test.each(this.inputRuleMatches)(
+        "%p",
+        ({ editorInput, markdownOutput, proseMirrorNodes }) => {
+          expect.assertions(3);
 
-      for (const { editorInput, markdownOutput, proseMirrorNodes } of this
-        .inputRuleMatches) {
-        const source = "BEGIN";
-        const proseMirrorRoot = this.pmu.parse(source);
-        const proseMirrorTree = this.pmu
-          .schema()
-          .nodes[
-            "doc"
-          ].createAndFill({}, [this.pmu.schema().nodes["paragraph"].createAndFill({}, [this.pmu.schema().text("BEGIN"), ...proseMirrorNodes, this.pmu.schema().text("END")])!])!;
+          const source = "BEGIN";
+          const proseMirrorRoot = this.pmu.parse(source);
+          const proseMirrorTree = this.pmu
+            .schema()
+            .nodes[
+              "doc"
+            ].createAndFill({}, [this.pmu.schema().nodes["paragraph"].createAndFill({}, [this.pmu.schema().text("BEGIN"), ...proseMirrorNodes, this.pmu.schema().text("END")])!])!;
 
-        jest.spyOn(console, "warn").mockImplementation();
-        createEditor(proseMirrorRoot, {
-          plugins: [this.pmu.inputRulesPlugin()],
-        })
-          .selectText("end")
-          .insertText(editorInput)
-          .insertText("END")
-          .callback((content) => {
-            expect(content.doc).toEqualProsemirrorNode(proseMirrorTree);
-            expect(this.pmu.serialize(content.doc)).toBe(
-              `BEGIN${markdownOutput}END\n`,
-            );
-          });
+          jest.spyOn(console, "warn").mockImplementation();
+          createEditor(proseMirrorRoot, {
+            plugins: [this.pmu.inputRulesPlugin()],
+          })
+            .selectText("end")
+            .insertText(editorInput)
+            .insertText("END")
+            .callback((content) => {
+              expect(content.doc).toEqualProsemirrorNode(proseMirrorTree);
+              expect(this.pmu.serialize(content.doc)).toBe(
+                `BEGIN${markdownOutput}END\n`,
+              );
+            });
 
-        // eslint-disable-next-line no-console -- Testing for console
-        expect(console.warn).not.toHaveBeenCalled();
-      }
+          // eslint-disable-next-line no-console -- Testing for console
+          expect(console.warn).not.toHaveBeenCalled();
+        },
+      );
     });
   }
 
